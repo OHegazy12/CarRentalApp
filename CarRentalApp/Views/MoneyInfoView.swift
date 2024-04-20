c3da7c43afca895d434271c9270bb820e53b93fa
Omar Hegazy <omarhegazy78@gmail.com>
2024-04-20 17:09:03 -0400

    update

--- CarRentalApp/Views/MoneyInfoView.swift
+++ CarRentalApp/Views/MoneyInfoView.swift
@@ -62,72 +62,70 @@ struct PieView: View {
             CarChartData(name: car.name, pricePerDay: car.pricePerDay, color: Color(hex: car.color))
         }
         
-        ScrollView {
-            VStack {
-                Text("Money Breakdown")
-                    .font(.title)
-                    .fontWeight(.heavy)
+        VStack {
+            Text("Money Breakdown")
+                .font(.title)
+                .fontWeight(.heavy)
+            
+            Canvas { context, size in
+                let total = data.reduce(0) {$0 + $1.pricePerDay}
                 
-                Canvas { context, size in
-                    let total = data.reduce(0) {$0 + $1.pricePerDay}
-                    
-                    if(displayDonutChart) {
-                        let donut = Path{ p in
-                            p.addEllipse(in: CGRect(origin: .zero, size: size))
-                            p.addEllipse(in: CGRect(x: size.width * 0.25, y: size.height * 0.25, width: size.width * 0.5, height: size.height * 0.5))
-                        }
-                        context.clip(to: donut, style: .init(eoFill: true))
+                if(displayDonutChart) {
+                    let donut = Path{ p in
+                        p.addEllipse(in: CGRect(origin: .zero, size: size))
+                        p.addEllipse(in: CGRect(x: size.width * 0.25, y: size.height * 0.25, width: size.width * 0.5, height: size.height * 0.5))
                     }
-                    
-                    context.translateBy(x: size.width*0.5, y: size.height*0.5)
-                    var picontext = context
-                    picontext.rotate(by: .degrees(-90))
-                    let radius = min(size.width, size.height)*0.45
-                    var startAngle = Angle.zero
-                    for carData in data {
-                        let angle = Angle(degrees: 360 * (carData.pricePerDay/total))
-                        let endAngle = startAngle + angle
-                        let path = Path { p in
-                            p.move(to: .zero)
-                            p.addArc(center: .zero, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
-                            p.closeSubpath()
-                        }
-                        
-                        picontext.fill(path , with: .color(carData.color))
-                        startAngle = endAngle
-                    }
-                }
-                .aspectRatio(contentMode: .fit)
-                VStack(alignment: .center) {
-                    ForEach(data) { carData in
-                        HStack {
-                            Rectangle()
-                                .frame(width: 30, height: 20)
-                                .foregroundColor(carData.color)
-                            Text(carData.name)
-                                .font(.footnote)
-                            Text("$\(String(format: "%.2f", carData.pricePerDay)) per day")
-                                .font(.caption)
-                        }
-                    }
-                    
-                    
-                    Text("Monthly Revenue for \(months[selectedMonth]): $\(String(format: "%.2f", monthlyRevenue))")
-                        .font(.headline)
-                    
-                    
-                    Picker("Select Month", selection: $selectedMonth) {
-                        ForEach(0..<months.count) { index in
-                            Text(months[index]).tag(index)
-                        }
-                    }
-                    .pickerStyle(MenuPickerStyle())
-                    .frame(maxWidth: .infinity)
+                    context.clip(to: donut, style: .init(eoFill: true))
                 }
                 
-                Toggle("Display Donut Chart?", isOn: $displayDonutChart)
-                    .padding()
+                context.translateBy(x: size.width*0.5, y: size.height*0.5)
+                var picontext = context
+                picontext.rotate(by: .degrees(-90))
+                let radius = min(size.width, size.height)*0.45
+                var startAngle = Angle.zero
+                for carData in data {
+                    let angle = Angle(degrees: 360 * (carData.pricePerDay/total))
+                    let endAngle = startAngle + angle
+                    let path = Path { p in
+                        p.move(to: .zero)
+                        p.addArc(center: .zero, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+                        p.closeSubpath()
+                    }
+                    
+                    picontext.fill(path , with: .color(carData.color))
+                    startAngle = endAngle
+                }
             }
+            .aspectRatio(contentMode: .fit)
+            VStack(alignment: .center) {
+                ForEach(data) { carData in
+                    HStack {
+                        Rectangle()
+                            .frame(width: 30, height: 20)
+                            .foregroundColor(carData.color)
+                        Text(carData.name)
+                            .font(.footnote)
+                        Text("$\(String(format: "%.2f", carData.pricePerDay)) per day")
+                            .font(.caption)
+                    }
+                }
+                
+                
+                Text("Monthly Revenue for \(months[selectedMonth]): $\(String(format: "%.2f", monthlyRevenue))")
+                    .font(.headline)
+                
+                
+                Picker("Select Month", selection: $selectedMonth) {
+                    ForEach(0..<months.count) { index in
+                        Text(months[index]).tag(index)
+                    }
+                }
+                .pickerStyle(MenuPickerStyle())
+                .frame(maxWidth: .infinity)
+            }
+            
+            Toggle("Display Donut Chart?", isOn: $displayDonutChart)
+                .padding()
         }
     }
 }
